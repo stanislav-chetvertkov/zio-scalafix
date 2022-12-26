441f8ec09f063d86a4d2b93f9cb09fe50f0240d2
schetvertkov <schetvertkov@twilio.com>
2022-12-26 14:35:21 +0200

    zio ignore rule

--- input/src/main/scala/fix/ZioUnit.scala
+++ input/src/main/scala/fix/ZioUnit.scala
@@ -6,4 +6,7 @@ package fix
 import zio.ZIO
 object ZioUnit {
   ZIO.succeed("test") *> ZIO.unit
+  ZIO.succeed("test2").map(_ => ())
+  val test2 = ZIO.succeed("test2")
+  test2.map(_ => ())
 }
--- output/src/main/scala/fix/ZioUnit.scala
+++ output/src/main/scala/fix/ZioUnit.scala
@@ -3,4 +3,7 @@ package fix
 import zio.ZIO
 object ZioUnit {
   ZIO.succeed("test").unit
+  ZIO.succeed("test2").unit
+  val test2 = ZIO.succeed("test2")
+  test2.unit
 }
